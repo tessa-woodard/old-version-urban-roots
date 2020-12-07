48353facad5885a42ecb53a2e84c3df561a936f1
Tessa Woodard <tessawoodard@outlook.com>
2020-12-06 22:52:50 -0500

    wip

--- backend/model/Products.js
+++ backend/model/Products.js
@@ -35,7 +35,8 @@ Product.create(
   {
     name: 'Succulent Dish Garden',
     price: 59.99,
-    image: 'https://www.plants.com/images/r_177068S_20200929-1601407137754.jpg',
+    image:
+      'https://github.com/tessa-woodard/urban-roots-revised/blob/main/src/assets/plants/dish-garden.jpg?raw=true',
     id: 1,
     category: 'Succulents'
   },
@@ -43,7 +44,7 @@ Product.create(
     name: 'Wood Succulent Garden',
     price: 59.99,
     image:
-      'https://www.plants.com/images/r_RECLAIMED_20191121-1574352224619.png',
+      'https://github.com/tessa-woodard/urban-roots-revised/blob/main/src/assets/plants/wood-garden.png?raw=true',
     id: 2,
     category: 'Succulents'
   },
@@ -51,44 +52,45 @@ Product.create(
     name: 'Black Echevaria Succulent',
     price: 25.99,
     image:
-      'https://www.plants.com/images/r_177066BLK_20200929-1601408271520.jpg',
+      'https://github.com/tessa-woodard/urban-roots-revised/blob/main/src/assets/plants/echevaria.jpg?raw=true',
     id: 3,
     category: 'Succulents'
   },
   {
     name: 'Lula Garden Succulents',
     price: 54.99,
-    image: 'https://www.plants.com/images/r_large_20200604-1591280842606.jpeg',
-    id: 3,
-    category: 'Succulents'
-  },
-  {
-    name: 'Flaming Katy',
-    price: 17,
     image:
-      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+      'https://github.com/tessa-woodard/urban-roots-revised/blob/main/src/assets/plants/lula.jpeg?raw=true',
     id: 3,
     category: 'Succulents'
   },
   {
-    name: 'Flaming Katy',
-    price: 17,
+    name: 'Succulent Garden',
+    price: 29.99,
     image:
-      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+      'https://www.plants.com/images/r_preview%20new_20200117-1579282468048.jpeg',
     id: 3,
     category: 'Succulents'
   },
   {
-    name: 'Flaming Katy',
-    price: 17,
+    name: 'Safari Animal Succulents',
+    price: 29.99,
     image:
-      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+      'https://www.plants.com/images/r_177604ger_20201027-1603831514550.jpg',
     id: 3,
     category: 'Succulents'
   },
   {
-    name: 'Flaming Katy',
-    price: 17,
+    name: 'Wood Rectangle Succulents',
+    price: 49.99,
+    image:
+      'https://www.plants.com/images/r_1566419432375_20190821-1566419433339_20191127-1574831465024.png',
+    id: 3,
+    category: 'Succulents'
+  },
+  {
+    name: 'Rose Garden Succulent',
+    price: 32.99,
     image:
       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
     id: 3,
