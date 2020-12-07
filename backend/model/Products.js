57714828e7f3ff4533c3545e0e70823d5003da7f
Tessa Woodard <tessawoodard@outlook.com>
2020-12-06 22:37:47 -0500

    wip

--- backend/model/Products.js
+++ backend/model/Products.js
@@ -31,79 +31,78 @@ const productSchema = new mongoose.Schema({
 //Initial database structure. These are the products.
 const Product = mongoose.model('Product', productSchema)
 
-// Product.create(
-//   {
-//     name: 'Echeveria Laui',
-//     price: 12,
-//     image:
-//       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
-//     id: 1,
-//     category: 'Succulents'
-//   },
-//   {
-//     name: 'Ghost Plant',
-//     price: 14,
-//     image:
-//       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
-//     id: 2,
-//     category: 'Succulents'
-//   },
-//   {
-//     name: 'Flaming Katy',
-//     price: 17,
-//     image:
-//       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
-//     id: 3,
-//     category: 'Succulents'
-//   },
-//   {
-//     name: 'Flaming Katy',
-//     price: 17,
-//     image:
-//       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
-//     id: 3,
-//     category: 'Succulents'
-//   },
-//   {
-//     name: 'Flaming Katy',
-//     price: 17,
-//     image:
-//       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
-//     id: 3,
-//     category: 'Succulents'
-//   },
-//   {
-//     name: 'Flaming Katy',
-//     price: 17,
-//     image:
-//       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
-//     id: 3,
-//     category: 'Succulents'
-//   },
-//   {
-//     name: 'Flaming Katy',
-//     price: 17,
-//     image:
-//       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
-//     id: 3,
-//     category: 'Succulents'
-//   },
-//   {
-//     name: 'Flaming Katy',
-//     price: 17,
-//     image:
-//       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
-//     id: 3,
-//     category: 'Succulents'
-//   },
-//   {
-//     name: 'Flaming Katy',
-//     price: 17,
-//     image:
-//       'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
-//     id: 3,
-//     category: 'Succulents'
-//   }
-// )
+Product.create(
+  {
+    name: 'Succulent Dish Garden',
+    price: 59.99,
+    image: 'https://www.plants.com/images/r_177068S_20200929-1601407137754.jpg',
+    id: 1,
+    category: 'Succulents'
+  },
+  {
+    name: 'Ghost Plant',
+    price: 14,
+    image:
+      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+    id: 2,
+    category: 'Succulents'
+  },
+  {
+    name: 'Flaming Katy',
+    price: 17,
+    image:
+      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+    id: 3,
+    category: 'Succulents'
+  },
+  {
+    name: 'Flaming Katy',
+    price: 17,
+    image:
+      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+    id: 3,
+    category: 'Succulents'
+  },
+  {
+    name: 'Flaming Katy',
+    price: 17,
+    image:
+      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+    id: 3,
+    category: 'Succulents'
+  },
+  {
+    name: 'Flaming Katy',
+    price: 17,
+    image:
+      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+    id: 3,
+    category: 'Succulents'
+  },
+  {
+    name: 'Flaming Katy',
+    price: 17,
+    image:
+      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+    id: 3,
+    category: 'Succulents'
+  },
+  {
+    name: 'Flaming Katy',
+    price: 17,
+    image:
+      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+    id: 3,
+    category: 'Succulents'
+  },
+  {
+    name: 'Flaming Katy',
+    price: 17,
+    image:
+      'https://www.plants.com/images/r_1566418513060_20190821-1566418513838.png',
+    id: 3,
+    category: 'Succulents'
+  }
+)
 
 module.exports = Product
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -23,44 +23,13 @@ class Shop extends React.Component {
       })
   }
 
-  // displayProducts = (products) => {
-  //   return products.map((product, index) => (
-  //     <div key={index} className="col-lg-3 justify-content-center">
-  //       <div className="row">
-  //         <div className="card" style={{ minHeight: '100%' }}>
-  //           <div style={{ maxHeight: '500px' }}>
-  //             <img
-  //               src={product.image}
-  //               alt="plant-img"
-  //               className="card-img-top"
-  //             />
-  //           </div>
-  //           <div className="card-body text-center">
-  //             <div className="product-text">
-  //               <h1>{product.name}</h1>
-  //               <h1>${product.price}</h1>
-  //             </div>
-  //             <button type="button" class="btn btn-outline-dark">
-  //               Add To Cart
-  //             </button>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   ))
-  // }
-
-  // render() {
-  //   return <div>{this.displayProducts(this.state.products)}</div>
-  // }
-
   render() {
     return (
       <div className="container">
         <div className="row">
           {this.state.products.map((product, index) => (
             <div key={index} id="cardItem" className="col-xs-1 mx-5 my-5">
-              <div className="card" style={{ maxHeight: '100%' }}>
+              <div className="card" style={{ minHeight: '100%' }}>
                 <div style={{ maxHeight: '500px' }}>
                   <img src={product.image} alt="plant-img" />
                 </div>
@@ -81,39 +50,3 @@ class Shop extends React.Component {
 }
 
 export default Shop
-
-{
-  /* <div className="container">
-<div className="row">
-  {this.state.films.map(film => (
-    <div key={film.id} id='cardItem' className="col-xs-1">
-      <MovieCard film={film} />
-    </div>
-  ))}
-</div>
-</div> */
-}
-
-{
-  /* <div className="col-10 col-sm-8 col-md-6 col-lg-4  mx-auto my-3">
-      <div className="card" style={{ minHeight: "100%" }}>
-        <div style={{ maxHeight: "500px" }}>
-          <Img fluid={product.image.fluid} className="card-img-top" />
-          <div className="card-body text-center">
-            <h6>{product.title}</h6>
-            <h6>${product.price}</h6>
-            <button
-              className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
-              data-item-id={product.id}
-              data-item-name={product.title}
-              data-item-price={product.price}
-              data-item-image={product.image.fluid.src}
-              data-item-url="https://impresso-expresso.netlify.app/products"
-            >
-              Add To Cart
-            </button>
-          </div>
-        </div>
-      </div>
-    </div> */
-}
