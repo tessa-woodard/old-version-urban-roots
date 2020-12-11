4cb37e355c2e85566a1abff7e607bc3679bab2fd
Tessa Woodard <tessawoodard@outlook.com>
2020-12-10 19:35:45 -0500

    wip

--- src/components/AllProducts/Shop/Shop.js
+++ src/components/AllProducts/Shop/Shop.js
@@ -30,18 +30,25 @@ class Shop extends React.Component {
         <div className="row">
           {this.state.products.map((product) => (
             <div id="cardItem" className="col-xs-1 mx-5 my-5">
-              <div className="card" style={{ minHeight: '100%' }}>
+              <div
+                className="card"
+                style={{
+                  border: 'none',
+                  minHeight: '100%'
+                }}
+              >
                 <div style={{ maxHeight: '500px' }}>
                   <Link to={`/product/${product._id}`}>
-                    <img src={product.image} alt="" />
+                    <img
+                      src={product.image}
+                      alt=""
+                      style={{ borderRadius: '15px' }}
+                    />
                   </Link>
                 </div>
                 <div className="card-body text-center">
                   <h1 className="product-title">{product.title}</h1>
-                  <h1 className="product-title">${product.price}</h1>
-                  <button type="button" class="btn btn-outline-dark">
-                    Add To Cart
-                  </button>
+                  <h1 className="product-price">${product.price}</h1>
                 </div>
               </div>
             </div>
