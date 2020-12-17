40ff67a95efc4276fe15123dabc43633c0e81398
Tessa Woodard <tessawoodard@outlook.com>
2020-12-16 19:37:41 -0500

    wip

--- backend/server/index.js
+++ backend/server/index.js
@@ -14,22 +14,29 @@ const productRoutes = require('../routes/product')
 // route middlewares
 app.use('/api/products', productRoutes)
 
-app.listen(process.env.SERVER_PORT, (err) => {
+app.listen(process.env.REACT_APP_SERVER_PORT, (err) => {
   if (err) console.log('Error in server setup')
-  console.log(`👨 To Infinity & Beyond on Port => ${process.env.SERVER_PORT}`),
+  console.log(
+    `👨 To Infinity & Beyond on Port => ${process.env.REACT_APP_SERVER_PORT}`
+  ),
     mongoose.connect(
-      process.env.DB_CONNECT,
+      'mongodb+srv://tessa_w:Lovely567!@cluster0.zlszm.mongodb.net/UrbanRoots?retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
       },
-      () => console.log(`Our oven is set to ${process.env.SERVER_PORT} deg`)
+      () =>
+        console.log(
+          `Our oven is set to ${process.env.REACT_APP_SERVER_PORT} deg`
+        )
     )
   mongoose.connection.on('error', (e) => {
     console.log('mongo connect error!')
   })
   mongoose.connection.on('connected', () => {
-    console.log(`🚀 DB Blasting Off in ${process.env.SERVER_PORT} seconds`)
+    console.log(
+      `🚀 DB Blasting Off in ${process.env.REACT_APP_SERVER_PORT} seconds`
+    )
   })
 })
